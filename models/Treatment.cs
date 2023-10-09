75ba78b3b6b279292abf5f6e014a3ab1953b0d4b
Shaul Amran <shaul@swimm.io>
2023-10-09 19:05:31 +0300

    Auto sync treatment

--- models/Treatment.cs
+++ models/Treatment.cs
@@ -3,12 +3,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RepositoryPattern.models
 {
-    [Table("treatment")]
+    [Table("treatment20231009160530")]
     public class Treatment
     {
-        [Column("treatmentid202310091514002023100915080220231009150122")]
+        [Column("treatment20231009160530id202310091514002023100915080220231009150122")]
         public int Id { get; set; }
-        [Column("treatmenttext")]
+        [Column("treatment20231009160530text")]
         public string Text { get; set; }
         [Column("price")]
         public int Price { get; set; }
