14bd4bea8030bc67578481a22ccadd475699646e
Shaul Amran <shaul@swimm.io>
2023-10-09 17:22:16 +0300

    more changes

--- models/makeAutosyncableChange.js
+++ models/makeAutosyncableChange.js
@@ -1,32 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
 var child_process_1 = require("child_process");
+var fs = require("fs");
 var uuid_1 = require("uuid");
-var _a = process.argv.slice(2), fileName = _a[0], stringToReplace = _a[1], _b = _a[2], mainBranchName = _b === void 0 ? 'main' : _b;
+var fileName = process.argv[2];
+var stringToReplace = process.argv[3];
+var mainBranchName = process.argv[4] || 'main';
 if (!fileName || !stringToReplace) {
-    console.error('Usage: node script.js <fileName> <stringToReplace> [mainBranchName]');
+    console.error('Usage: node script.js <fileName> <stringToReplace> [<mainBranchName>]');
     process.exit(1);
 }
-var content = fs.readFileSync(fileName, 'utf-8');
-if (!content.includes(stringToReplace)) {
-    console.error("Error: String '".concat(stringToReplace, "' not found in file."));
-    process.exit(1);
-}
-var replacedContent = content.replace(new RegExp("".concat(stringToReplace, "-[0-9a-f-]+"), 'g'), "".concat(stringToReplace, "-").concat((0, uuid_1.v4)()));
-fs.writeFileSync(fileName, replacedContent, 'utf-8');
 try {
+    var fileContent = fs.readFileSync(fileName, 'utf8');
+    if (!fileContent.includes(stringToReplace)) {
+        throw new Error("String \"".concat(stringToReplace, "\" not found in the file."));
+    }
+    var uuidValue = (0, uuid_1.v4)();
+    var replacedContent = fileContent.replace(new RegExp(stringToReplace, 'g'), "".concat(stringToReplace, "-").concat(uuidValue));
+    fs.writeFileSync(fileName, replacedContent, 'utf8');
+    var tempBranchName = "temp-autosync-branch-".concat((new Date()).toISOString().split('T')[0].replace(/-/g, ''), "-").concat(uuidValue);
     (0, child_process_1.execSync)("git checkout ".concat(mainBranchName));
-    var dateOfToday = new Date().toISOString().slice(0, 16).replace(/[-T:]/g, '-');
-    var tempBranchName = "temp-autosync-branch-".concat(dateOfToday, "-").concat((0, uuid_1.v4)());
     (0, child_process_1.execSync)("git checkout -b ".concat(tempBranchName));
     (0, child_process_1.execSync)("git add ".concat(fileName));
-    (0, child_process_1.execSync)("git commit -m \"Auto-sync: Replace ".concat(stringToReplace, " with ").concat(stringToReplace, "-<uuid>\""));
+    (0, child_process_1.execSync)("git commit -m \"Auto sync changes\"");
     (0, child_process_1.execSync)("git push origin ".concat(tempBranchName));
     (0, child_process_1.execSync)("git checkout ".concat(mainBranchName));
     (0, child_process_1.execSync)("git pull");
+    console.log('Script executed successfully.');
 }
 catch (error) {
-    console.error("Error: ".concat(error.message));
+    console.error(error.message);
     process.exit(1);
 }
