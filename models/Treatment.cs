507ff11646773d6bd41de1ce9d6f7c24b4195337
Shaul Amran <shaul@swimm.io>
2023-10-09 17:44:00 +0300

    Auto sync: Replaced treatmentid

--- models/Treatment.cs
+++ models/Treatment.cs
@@ -6,7 +6,7 @@ namespace RepositoryPattern.models
     [Table("treatment")]
     public class Treatment
     {
-        [Column("treatmentid")]
+        [Column("treatmentid-6e0e5fb9-61e7-4305-ace2-ba51297cc5a8")]
         public int Id { get; set; }
         [Column("treatmenttext")]
         public string Text { get; set; }
