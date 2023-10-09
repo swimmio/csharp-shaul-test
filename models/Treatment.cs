d17db3d2a7536ffe053a4fbbaba61a9522e38a60
Shaul Amran <shaul@swimm.io>
2023-10-09 14:55:24 +0300

    test7

--- models/Treatment.cs
+++ models/Treatment.cs
@@ -6,7 +6,7 @@ namespace RepositoryPattern.models
     [Table("treatment")]
     public class Treatment
     {
-        [Column("treatmentid")]
+        [Column("treatmentid7")]
         public int Id { get; set; }
         [Column("treatmenttext")]
         public string Text { get; set; }
