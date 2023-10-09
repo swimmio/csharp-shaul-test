d89288d24b2846237cb569896a19c39d6ac7c769
Shaul Amran <shaul@swimm.io>
2023-10-09 16:32:14 +0300

    Commiting changes

--- models/makeAutosyncableChange.js
+++ models/makeAutosyncableChange.js
@@ -1,26 +1,45 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
-function replaceString(fileName, stringToReplace) {
-    // Check if the file exists
-    if (!fs.existsSync(fileName)) {
-        console.error("Error: File '".concat(fileName, "' not found."));
-        return;
+var child_process_1 = require("child_process");
+function replaceString(fileName, stringToReplace, mainBranchName) {
+    var content = fs.readFileSync(fileName, 'utf-8');
+    var regex = new RegExp("".concat(stringToReplace, "(\\d*)"), 'g');
+    var matches = content.match(regex);
+    if (!matches) {
+        throw new Error("String \"".concat(stringToReplace, "\" not found in the file."));
     }
-    // Get the current time in milliseconds
-    var currentTime = new Date().getTime();
-    // Read the file content
-    var content = fs.readFileSync(fileName, 'utf8');
-    // Replace the string
-    content = content.replace(new RegExp(stringToReplace, 'g'), "".concat(stringToReplace).concat(currentTime));
-    // Write the updated content back to the file
-    fs.writeFileSync(fileName, content);
-    console.log("String '".concat(stringToReplace, "' replaced with '").concat(stringToReplace).concat(currentTime, "' in '").concat(fileName, "'."));
+    var maxNumber = 0;
+    matches.forEach(function (match) {
+        var num = parseInt(match.replace(stringToReplace, ''), 10);
+        if (!isNaN(num) && num > maxNumber) {
+            maxNumber = num;
+        }
+    });
+    var replacementNumber = maxNumber + 1;
+    var updatedContent = content.replace(regex, "".concat(stringToReplace).concat(replacementNumber));
+    fs.writeFileSync(fileName, updatedContent, 'utf-8');
+    console.log("String \"".concat(stringToReplace, "\" replaced successfully."));
+    if (mainBranchName) {
+        try {
+            (0, child_process_1.execSync)("git checkout -b temp-autosync-branch-".concat(replacementNumber));
+            (0, child_process_1.execSync)('git add .');
+            (0, child_process_1.execSync)("git commit -m \"Auto-sync: Replaced ".concat(stringToReplace, " with ").concat(stringToReplace).concat(replacementNumber, "\""));
+            (0, child_process_1.execSync)('git push origin HEAD');
+            (0, child_process_1.execSync)("git checkout ".concat(mainBranchName));
+            (0, child_process_1.execSync)("git pull origin ".concat(mainBranchName));
+        }
+        catch (error) {
+            console.error('Error in Git operations:', error.message);
+            process.exit(1);
+        }
+    }
 }
 var fileName = process.argv[2];
 var stringToReplace = process.argv[3];
+var mainBranchName = process.argv[4];
 if (!fileName || !stringToReplace) {
-    console.error("Usage: node replace_string.ts <fileName> <stringToReplace>");
+    console.error('Please provide both fileName and stringToReplace as arguments.');
     process.exit(1);
 }
-replaceString(fileName, stringToReplace);
+replaceString(fileName, stringToReplace, mainBranchName);
--- models/makeAutosyncableChange.ts
+++ models/makeAutosyncableChange.ts
@@ -1,33 +1,55 @@
 import * as fs from 'fs';
+import { execSync } from 'child_process';
 
-function replaceString(fileName: string, stringToReplace: string): void {
-    // Check if the file exists
-    if (!fs.existsSync(fileName)) {
-        console.error(`Error: File '${fileName}' not found.`);
-        return;
+function replaceString(fileName: string, stringToReplace: string, mainBranchName?: string) {
+    const content = fs.readFileSync(fileName, 'utf-8');
+    const regex = new RegExp(`${stringToReplace}(\\d*)`, 'g');
+    const matches = content.match(regex);
+
+    if (!matches) {
+        throw new Error(`String "${stringToReplace}" not found in the file.`);
     }
 
-    // Get the current time in milliseconds
-    const currentTime = new Date().getTime();
+    let maxNumber = 0;
+    matches.forEach(match => {
+        const num = parseInt(match.replace(stringToReplace, ''), 10);
+        if (!isNaN(num) && num > maxNumber) {
+            maxNumber = num;
+        }
+    });
 
-    // Read the file content
-    let content = fs.readFileSync(fileName, 'utf8');
+    const replacementNumber = maxNumber + 1;
 
-    // Replace the string
-    content = content.replace(new RegExp(stringToReplace, 'g'), `${stringToReplace}${currentTime}`);
+    const updatedContent = content.replace(
+        regex,
+        `${stringToReplace}${replacementNumber}`
+    );
 
-    // Write the updated content back to the file
-    fs.writeFileSync(fileName, content);
+    fs.writeFileSync(fileName, updatedContent, 'utf-8');
+    console.log(`String "${stringToReplace}" replaced successfully.`);
 
-    console.log(`String '${stringToReplace}' replaced with '${stringToReplace}${currentTime}' in '${fileName}'.`);
+    if (mainBranchName) {
+        try {
+            execSync(`git checkout -b temp-autosync-branch-${replacementNumber}`);
+            execSync('git add .');
+            execSync(`git commit -m "Auto-sync: Replaced ${stringToReplace} with ${stringToReplace}${replacementNumber}"`);
+            execSync('git push origin HEAD');
+            execSync(`git checkout ${mainBranchName}`);
+            execSync(`git pull origin ${mainBranchName}`);
+        } catch (error) {
+            console.error('Error in Git operations:', error.message);
+            process.exit(1);
+        }
+    }
 }
 
 const fileName = process.argv[2];
 const stringToReplace = process.argv[3];
+const mainBranchName = process.argv[4];
 
 if (!fileName || !stringToReplace) {
-    console.error("Usage: node replace_string.ts <fileName> <stringToReplace>");
+    console.error('Please provide both fileName and stringToReplace as arguments.');
     process.exit(1);
 }
 
-replaceString(fileName, stringToReplace);
+replaceString(fileName, stringToReplace, mainBranchName);
