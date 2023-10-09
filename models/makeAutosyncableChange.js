7d8b735a2587f05c3cea6fc7600831750bb5b4d7
Shaul Amran <shaul@swimm.io>
2023-10-09 17:16:35 +0300

    adding git ignore

--- models/makeAutosyncableChange.js
+++ models/makeAutosyncableChange.js
@@ -1,59 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
-var child_process = require("child_process");
-function replaceString(fileName, stringToReplace, mainBranchName) {
-    if (mainBranchName === void 0) { mainBranchName = 'main'; }
-    var content = fs.readFileSync(fileName, 'utf-8');
-    var regex = new RegExp("".concat(stringToReplace, "(\\d+)"), 'g');
-    var matches = content.match(regex);
-    if (!matches) {
-        throw new Error("String \"".concat(stringToReplace, "\" not found in the file."));
-    }
-    var maxNumber = 0;
-    matches.forEach(function (match) {
-        var num = parseInt(match.replace(stringToReplace, ''), 10);
-        if (!isNaN(num) && num > maxNumber) {
-            maxNumber = num;
-        }
-    });
-    var replacementNumber = maxNumber + 1;
-    var updatedContent = content.replace(regex, "".concat(stringToReplace).concat(replacementNumber));
-    fs.writeFileSync(fileName, updatedContent, 'utf-8');
-    console.log("String \"".concat(stringToReplace, "\" replaced successfully."));
-    // Remember replacementNumber for later use
-    return replacementNumber;
-}
-function executeCommand(command) {
-    var result = child_process.execSync(command, { encoding: 'utf-8' });
-    console.log(result);
-}
-var fileName = process.argv[2];
-var stringToReplace = process.argv[3];
-var mainBranchName = process.argv[4] || 'main';
+var child_process_1 = require("child_process");
+var uuid_1 = require("uuid");
+var _a = process.argv.slice(2), fileName = _a[0], stringToReplace = _a[1], _b = _a[2], mainBranchName = _b === void 0 ? 'main' : _b;
 if (!fileName || !stringToReplace) {
-    console.error('Please provide both fileName and stringToReplace as arguments.');
+    console.error('Usage: node script.js <fileName> <stringToReplace> [mainBranchName]');
     process.exit(1);
 }
+var content = fs.readFileSync(fileName, 'utf-8');
+if (!content.includes(stringToReplace)) {
+    console.error("Error: String '".concat(stringToReplace, "' not found in file."));
+    process.exit(1);
+}
+var replacedContent = content.replace(new RegExp("".concat(stringToReplace, "-[0-9a-f-]+"), 'g'), "".concat(stringToReplace, "-").concat((0, uuid_1.v4)()));
+fs.writeFileSync(fileName, replacedContent, 'utf-8');
 try {
-    var replacementNumber = replaceString(fileName, stringToReplace, mainBranchName);
-    // Checkout to main branch
-    executeCommand("git checkout ".concat(mainBranchName));
-    // Get today's date in the format yyyy-mm-dd
-    var today = new Date().toISOString().slice(0, 10);
-    // Create and checkout a new branch
-    var newBranchName = "temp-autosync-branch-".concat(today, "-").concat(replacementNumber);
-    executeCommand("git checkout -b ".concat(newBranchName));
-    // Commit changes to the new branch
-    executeCommand("git add .");
-    executeCommand("git commit -m \"Auto-synced ".concat(stringToReplace, "\""));
-    // Push changes to origin
-    executeCommand("git push origin ".concat(newBranchName));
-    // Switch back to main branch and pull
-    executeCommand("git checkout ".concat(mainBranchName));
-    executeCommand("git pull");
+    (0, child_process_1.execSync)("git checkout ".concat(mainBranchName));
+    var dateOfToday = new Date().toISOString().slice(0, 16).replace(/[-T:]/g, '-');
+    var tempBranchName = "temp-autosync-branch-".concat(dateOfToday, "-").concat((0, uuid_1.v4)());
+    (0, child_process_1.execSync)("git checkout -b ".concat(tempBranchName));
+    (0, child_process_1.execSync)("git add ".concat(fileName));
+    (0, child_process_1.execSync)("git commit -m \"Auto-sync: Replace ".concat(stringToReplace, " with ").concat(stringToReplace, "-<uuid>\""));
+    (0, child_process_1.execSync)("git push origin ".concat(tempBranchName));
+    (0, child_process_1.execSync)("git checkout ".concat(mainBranchName));
+    (0, child_process_1.execSync)("git pull");
 }
 catch (error) {
-    console.error(error.message);
+    console.error("Error: ".concat(error.message));
     process.exit(1);
 }
--- models/makeAutosyncableChange.ts
+++ models/makeAutosyncableChange.ts
@@ -1,79 +1,35 @@
 import * as fs from 'fs';
-import * as child_process from 'child_process';
+import { execSync } from 'child_process';
+import { v4 as uuidv4 } from 'uuid';
 
-function replaceString(
-  fileName: string,
-  stringToReplace: string,
-  mainBranchName: string = 'main'
-) {
-  const content = fs.readFileSync(fileName, 'utf-8');
-  const regex = new RegExp(`${stringToReplace}(\\d+)`, 'g');
-  const matches = content.match(regex);
-
-  if (!matches) {
-    throw new Error(`String "${stringToReplace}" not found in the file.`);
-  }
-
-  let maxNumber = 0;
-  matches.forEach(match => {
-    const num = parseInt(match.replace(stringToReplace, ''), 10);
-    if (!isNaN(num) && num > maxNumber) {
-      maxNumber = num;
-    }
-  });
-
-  const replacementNumber = maxNumber + 1;
-
-  const updatedContent = content.replace(
-    regex,
-    `${stringToReplace}${replacementNumber}`
-  );
-
-  fs.writeFileSync(fileName, updatedContent, 'utf-8');
-  console.log(`String "${stringToReplace}" replaced successfully.`);
-
-  // Remember replacementNumber for later use
-  return replacementNumber;
-}
-
-function executeCommand(command: string): void {
-  const result = child_process.execSync(command, { encoding: 'utf-8' });
-  console.log(result);
-}
-
-const fileName = process.argv[2];
-const stringToReplace = process.argv[3];
-const mainBranchName = process.argv[4] || 'main';
+const [fileName, stringToReplace, mainBranchName = 'main'] = process.argv.slice(2);
 
 if (!fileName || !stringToReplace) {
-  console.error('Please provide both fileName and stringToReplace as arguments.');
+  console.error('Usage: node script.js <fileName> <stringToReplace> [mainBranchName]');
   process.exit(1);
 }
 
+const content = fs.readFileSync(fileName, 'utf-8');
+
+if (!content.includes(stringToReplace)) {
+  console.error(`Error: String '${stringToReplace}' not found in file.`);
+  process.exit(1);
+}
+
+const replacedContent = content.replace(new RegExp(`${stringToReplace}-[0-9a-f-]+`, 'g'), `${stringToReplace}-${uuidv4()}`);
+fs.writeFileSync(fileName, replacedContent, 'utf-8');
+
 try {
-  const replacementNumber = replaceString(fileName, stringToReplace, mainBranchName);
-
-  // Checkout to main branch
-  executeCommand(`git checkout ${mainBranchName}`);
-
-  // Get today's date in the format yyyy-mm-dd
-  const today = new Date().toISOString().slice(0, 10);
-
-  // Create and checkout a new branch
-  const newBranchName = `temp-autosync-branch-${today}-${replacementNumber}`;
-  executeCommand(`git checkout -b ${newBranchName}`);
-
-  // Commit changes to the new branch
-  executeCommand(`git add .`);
-  executeCommand(`git commit -m "Auto-synced ${stringToReplace}"`);
-
-  // Push changes to origin
-  executeCommand(`git push origin ${newBranchName}`);
-
-  // Switch back to main branch and pull
-  executeCommand(`git checkout ${mainBranchName}`);
-  executeCommand(`git pull`);
+  execSync(`git checkout ${mainBranchName}`);
+  const dateOfToday = new Date().toISOString().slice(0, 16).replace(/[-T:]/g, '-');
+  const tempBranchName = `temp-autosync-branch-${dateOfToday}-${uuidv4()}`;
+  execSync(`git checkout -b ${tempBranchName}`);
+  execSync(`git add ${fileName}`);
+  execSync(`git commit -m "Auto-sync: Replace ${stringToReplace} with ${stringToReplace}-<uuid>"`);
+  execSync(`git push origin ${tempBranchName}`);
+  execSync(`git checkout ${mainBranchName}`);
+  execSync(`git pull`);
 } catch (error) {
-  console.error(error.message);
+  console.error(`Error: ${error.message}`);
   process.exit(1);
 }
