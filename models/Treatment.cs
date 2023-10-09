c6331b03c3c06a9840cb6e4cf8855edfad227ebc
Shaul Amran <shaul@swimm.io>
2023-10-09 17:35:06 +0300

    Auto-sync changes

--- models/Treatment.cs
+++ models/Treatment.cs
@@ -6,7 +6,7 @@ namespace RepositoryPattern.models
     [Table("treatment")]
     public class Treatment
     {
-        [Column("treatmentid")]
+        [Column("treatmentid-0c6ab3a1-2e43-4046-a5d4-1b28e347e966")]
         public int Id { get; set; }
         [Column("treatmenttext")]
         public string Text { get; set; }
