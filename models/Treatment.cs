5944975186fd6a09b66076392358600b1140bda8
Shaul Amran <shaul@swimm.io>
2023-10-12 12:17:46 +0300

    3

--- models/Treatment.cs
+++ models/Treatment.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RepositoryPattern.models
 {
-    [Table("treatment2")]
+    [Table("treatment3")]
     public class Treatment
     {
         [Column("treatmentid")]
