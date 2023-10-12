8438cf2b0f13fa813569e4c7e2c577a0b7f6db98
Shaul Amran <shaul@swimm.io>
2023-10-12 12:12:48 +0300

    1

--- models/Treatment.cs
+++ models/Treatment.cs
@@ -3,12 +3,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RepositoryPattern.models
 {
-    [Table("treatment20231012091113")]
+    [Table("treatment1")]
     public class Treatment
     {
-        [Column("treatment20231012091113id202310091514002023100915080220231009150122")]
+        [Column("treatmentid")]
         public int Id { get; set; }
-        [Column("treatment20231012091113text")]
+        [Column("treatment")]
         public string Text { get; set; }
         [Column("price")]
         public int Price { get; set; }
