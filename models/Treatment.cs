7fdaef1cd7ce2be87c481337c31aecd2cbfa4a8e
Shaul Amran <shaul@swimm.io>
2023-10-16 04:49:47 -0400

    Update models/Treatment.cs

--- models/Treatment.cs
+++ models/Treatment.cs
@@ -8,7 +8,7 @@ namespace RepositoryPattern.models
     {
         [Column("treatment2023101608474420231012135252id202310091514002023100915080220231009150122")]
         public int Id { get; set; }
-        [Column("treatment2023101608474420231012135252text")]
+        [Column("treatment2023101608474420231012135252text11")]
         public string Text { get; set; }
         [Column("price")]
         public int Price { get; set; }
